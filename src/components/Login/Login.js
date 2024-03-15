70f4dd390022f36224ed147ed5d4503ae72a2bd1
Eallekors <erkxalle@gmail.com>
2024-03-15 11:32:29 +0200

    Invalid login state

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,26 +2,57 @@ import './Login.css'
 
 import Card from '../UI/Card'
 import Button from '../UI/Button'
+import { useState, useSyncExternalStore } from 'react'
 
 const Login = () => {
+    const [enteredEmail, setEnteredEmail] = useState('')
+    const [enteredPassword, setEnteredPassword] = useState('')
+    const [emailIsValid, setEmailIsValid] = useState('')
+    const [passwordIsValid, setPasswordIsValid] = useState('')
+
+    const emailChangeHandler = (event) => {
+        setEnteredEmail(event.target.value)
+    }
+
+    const passwordChangeHandler = (event) => {
+        setEnteredPassword(event.target.value)
+    }
+
+    const emailValidateHandler = () => {
+        setEmailIsValid(enteredEmail.includes('@'))
+    }
+
+    const passwordValidateHandler = () => {
+        setPasswordIsValid(enteredPassword.trim().length > 6)
+    }
+
+
     return (
         <Card className='login'>
             <form>
-                <div>
+                <div className={`control ${emailIsValid === false ? 
+'invalid' : ''}`}>
                     <label for='email'>Email</label>
                     <input 
                         type='email'
                         id='email'
+                        value={enteredEmail}
+                        onChange={emailChangeHandler}
+                        onBlur={emailValidateHandler}
                     ></input>
                 </div>
-                <div>
+                <div className={`control ${passwordIsValid === false ? 
+'invalid' : ''}`}>
                     <label for='password'>Password</label>
                     <input
                         type='password'
                         id='password'
+                        value={enteredPassword}
+                        onChange={passwordChangeHandler}
+                        onBlur={passwordValidateHandler}
                     ></input>
                 </div>
-                <div>
+                <div className='actions'>
                     <Button type='submit'>
                         Login
                     </Button>
